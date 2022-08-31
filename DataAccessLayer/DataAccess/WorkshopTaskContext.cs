9cad1e5e985bf2a0e108259207fb5349fbb4c034
PanterA989 <krzysztof.kania1@pollub.edu.pl>
2022-08-31 22:22:46 +0200

    Fixed missing SNI.dll error

---
+++ DataAccessLayer/DataAccess/WorkshopTaskContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Workshop.DataAccessLayer.DatabaseConnection;
+using Workshop.DataAccessLayer.Models;
+using Workshop.DataAccessLayer.Models.Dictionaries;
+
+namespace Workshop.DataAccessLayer.DataAccess
+{
+    public class WorkshopTaskContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionHelper.GetConnectionString());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<TaskModel> TaskModels { get; set; }
+        public DbSet<StatusModel> StatusModels { get; set; }
+
+    }
+}
