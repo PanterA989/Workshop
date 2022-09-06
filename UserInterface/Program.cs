0a31cc14e7bfe9fce8fcb47fad6a8e8a24e6daf2
PanterA989 <krzysztof.kania1@pollub.edu.pl>
2022-09-06 10:09:13 +0200

    Added automatic database migration

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.EntityFrameworkCore;
+using Workshop.DataAccessLayer.DataAccess;
 using Workshop.UserInterface.Forms;
 
 namespace UserInterface
@@ -15,6 +17,11 @@ namespace UserInterface
         [STAThread]
         static void Main()
         {
+            using (var dbContext = new WorkshopTaskContext())
+            {
+                dbContext.Database.Migrate();
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
